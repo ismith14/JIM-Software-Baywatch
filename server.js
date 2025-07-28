ce0f2f573f89bbc7884a2ef6ad01797ae153bc9c
Ian Smith <ismith14@students.towson.edu>
2025-07-28 03:07:24 -0400

    added css to login and account creation. Added the ability to display coordinates of user as text on screen and added some sources

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ let publicpath = path.join(__dirname)
 
 var user = ''
 
+//establishes database credentials
+//uses MySql
+//database was tested locally, requires locally run database to connect
 const con = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -15,6 +18,7 @@ const con = mysql.createConnection({
     database: 'baywatchdb'
 })
 
+//establish connection to database for the server
 con.connect((err) => {
     try{
         if(err){
@@ -30,11 +34,13 @@ app.use(express.static('public'))
 
 app.use(express.json())
 
+//default page for application
 app.get('/', (req, res) => {
     res.sendFile(`${publicpath}/public/frontPage.html`)
 })
 
-app.get('/main', (req, res) => {
+//retrieves all locations with the database and returns them to frontend for display in dropdown menu
+app.get('/api/main', (req, res) => {
     var sql = 'select locationName from locations'
     con.query(sql, (err, result) => {
         if(err) throw err;
@@ -45,7 +51,8 @@ app.get('/main', (req, res) => {
     })
 })
 
-app.get('/locationData/:locationName', (req, res)=>{
+//retrieves all data about a select location from database and returns data to frontend for display
+app.get('/api/locationData/:locationName', (req, res)=>{
     var locationName = req.params.locationName
     var sql = "select * from locations join datalogs on datalogs.locationName = locations.locationName where locations.locationName = ? order by logDate desc limit 1" 
     con.query(sql, [locationName], (err, result) => {
@@ -57,6 +64,7 @@ app.get('/locationData/:locationName', (req, res)=>{
     })
 })
 
+//performs queries for the username and password to see if the correct username and password were inputted
 app.post('/api/login', (req, res) =>{
     var sql = "select exists (select username from users where username = ? ) as usernameExists;"
     con.query(sql, [req.body.first], (err, result) =>{
@@ -80,7 +88,7 @@ app.post('/api/login', (req, res) =>{
         }
     })
 })
-
+//tries to insert new user data into database, if not unique then will give error to user
 app.post('/api/createUser', (req, res) =>{
     var sql = "Insert into users values ( ? , ? );"
         try{
@@ -97,22 +105,21 @@ app.post('/api/createUser', (req, res) =>{
         res.send({message: 'Duplicate Entry'})
     }
 })
-
+//gives the currently logged in user to frontend
 app.get('/api/getUser', (req, res) => {
     if(user == ''){
         res.send([{message: user}])
     }else{
-        console.log(user)
         res.send([{message: user}])
     }
 })
-
+//allows user to logout
 app.get('/api/userLogout', (req,res) =>{
     user = ''
     console.log(user)
     res.send([{message: user}])
 })
-
+//port the server.js listens to run locally
 app.listen(3000, () => {
     console.log('server listen on port 3000')
 })
